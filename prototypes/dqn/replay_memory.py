2f22cb15543179ba1fc7598eaf152abfb22d7629
Mark Trovinger <mark.trovinger@gmail.com>
2020-06-25 17:55:34 -0400

    added dqn code from dq papers and started changing agent

---
+++ prototypes/dqn/replay_memory.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+class ReplayBuffer():
+    def __init__(self, max_size, input_shape, n_actions):
+        self.memory_size = max_size
+        self.memory_counter = 0
+        self.state_memory = np.zeros((self.memory_size, *input_shape), dtype=np.float32)
+        self.new_state_memory = np.zeros((self.memory_size, *input_shape), dtype=np.float32)
+        self.action_memory = np.zeros(self.memory_size,  dtype=np.int64)
+        self.reward_memory = np.zeros(self.memory_size,  dtype=np.float32)
+        self.terminal_memory = np.zeros(self.memory_size,  dtype=np.uint8)
+
+    def store_transistion(self, state, action, reward, state_, done):
+        index = self.memory_counter % self.memory_size
+        self.state_memory[index] = state
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.new_state_memory[index] = state_
+        self.terminal_memory[index] = done
+        self.memory_counter += 1
+
+    def sample_buffer(self, batch_size):
+        max_mem = min(self.memory_counter, self.memory_size)
+
+        batch = np.random.choice(max_mem, batch_size, replace=False)
+
+        states = self.state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        states_ = self.new_state_memory[batch]
+        dones = self.terminal_memory[batch]
+
+        return states, actions, rewards, states_, dones
