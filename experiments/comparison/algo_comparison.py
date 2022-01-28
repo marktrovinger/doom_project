573ee8e68aeaa90abec935246cac482f90e6dc24
Mark Trovinger <mark.trovinger@gmail.com>
2022-01-27 19:51:49 -0500

    more framework

--- experiments/comparison/algo_comparison.py
+++ experiments/comparison/algo_comparison.py
@@ -1,7 +1,12 @@
 # imports, don't forget wandb
+import numpy as np
+from stable_baselines3 import DQN, PPO
+
 
 
 
 # main comparison, investigate how many timesteps are needed
 
-# 
+# write results to wand for that run
+
+# save model if performance is better
