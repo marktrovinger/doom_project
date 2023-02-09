071f92551fd19c1880b228fa33297be566fdc7fe
Perusha Moodley <perusha.moodley@gmail.com>
2023-02-09 15:39:04 +0000

    initial commit - starting points for a vizdoom wrapper for dopamine

---
+++ prototypes/vizdoom_dopamine/vizdoom_wrapper_test.py
@@ -0,0 +1,14 @@
+import gym
+from vizdoom import gym_wrapper  # noqa
+
+if __name__ == "__main__":
+    env = gym.make("VizdoomHealthGatheringSupreme-v0")
+
+    # Rendering random rollouts for ten episodes
+    for _ in range(10):
+        done = False
+        obs = env.reset()
+        while not done:
+            obs, rew, terminated, truncated, info = env.step(env.action_space.sample())
+            done = terminated or truncated
+            env.render()
