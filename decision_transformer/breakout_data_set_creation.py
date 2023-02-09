829be20eb325b8a65bbede584e918f4ce95156ea
Mark Trovinger <mark.trovinger@gmail.com>
2023-02-09 14:55:57 -0500

    first attempt at writing numpy arrays to gzip file

--- decision_transformer/breakout_data_set_creation.py
+++ decision_transformer/breakout_data_set_creation.py
@@ -1,4 +1,6 @@
 import gym
+import numpy as np
+import gzip
 
 from stable_baselines3 import DQN
 from stable_baselines3.common.env_util import make_atari_env
@@ -15,5 +17,13 @@ model.learn(total_timesteps=10000, log_interval=4)
 #del model # remove to demonstrate saving and loading
 
 #model = DQN.load("dqn_cartpole")
-model.save_replay_buffer("breakout_dqn_replay")
+# this takes way too much memory, let's see if we can do a better
+# job
+# model.save_replay_buffer("breakout_dqn_replay")
+
+model.replay_buffer.actions
+
+with gzip.open('', 'wb') as f:
+    f.write(np.save(model.replay_buffer.observations))
+    f.write(np.save(model.replay_buffer.actions))
 
